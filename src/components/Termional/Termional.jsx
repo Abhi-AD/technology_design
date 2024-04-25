1dc910c5a0849cba17fe58409fcebdf65740fbf2
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2024-04-25 23:41:25 +0545

    respsoive design

--- src/components/Termional/Termional.jsx
+++ src/components/Termional/Termional.jsx
@@ -5,26 +5,26 @@ import { RiDoubleQuotesR } from "react-icons/ri";
 
 const Termional = () => {
     return (
-        <div className="people-container">
-            <div className="people-image-holder">
-                <div className="people">
+        <div className="termional-container">
+            <div className="termional-image-holder">
+                <div className="termional">
                     <img src={person1} alt="" />
-                    <div className="people-des">
+                    <div className="termional-des">
                         <RiDoubleQuotesR className='quote-icon' />
                         <p className="bio1">Become an advocate for biotechnology and help shape policy that promotes innovation! Contact policymakers directly through BIO’s easy-to-use tool and have your voice heard.</p>
                         <p className="bio2">Over 40K subscribers worldwide can’t be wrong. Good Day BIO is the top resource for biotech industry news—and the only daily newsletter at the intersection of biotech, politics, and policy. (And, it’s free!) </p>
-                        <h1 className='people-name'>Shane Warne</h1>
-                        <p className='people-post'>Co-founder</p>
+                        <h1 className='termional-name'>Shane Warne</h1>
+                        <p className='termional-post'>Co-founder</p>
                     </div>
                 </div>
-                <div className="people">
+                <div className="termional">
                     <img src={person2} alt="" />
-                    <div className="people-des">
+                    <div className="termional-des">
                         <RiDoubleQuotesR className='quote-icon' />
                         <p className="bio1">Become an advocate for biotechnology and help shape policy that promotes innovation! Contact policymakers directly through BIO’s easy-to-use tool and have your voice heard.</p>
                         <p className="bio2">Over 40K subscribers worldwide can’t be wrong. Good Day BIO is the top resource for biotech industry news—and the only daily newsletter at the intersection of biotech, politics, and policy. (And, it’s free!) </p>
-                        <h1 className='people-name'>Shane Warne</h1>
-                        <p className='people-post'>Co-founder</p>
+                        <h1 className='termional-name'>Shane Warne</h1>
+                        <p className='termional-post'>Co-founder</p>
                     </div>
                 </div>
             </div>
