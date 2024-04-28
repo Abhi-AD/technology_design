0eb605b659dd7f670f0a16c4b5834aa2525cd886
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2024-04-28 23:32:54 +0545

    Visiting card design

--- src/page/VisitCard/CardDesign.jsx
+++ src/page/VisitCard/CardDesign.jsx
@@ -1,13 +1,66 @@
-import React from 'react'
-import './CardDesign.css'
-const CardDesign = () => {
-  return (
-    <div className='card-design'>
-      <div className="card">
-          <img src="https://img.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg?t=st=1714287276~exp=1714290876~hmac=9082e3bd254891b01591c960e6e2bab056bfdfdea732dcdb4e64c92e72aa3151&w=900" alt="" className='cardimg' />
-      </div>
-    </div>
-  )
-}
+import React, { useRef } from 'react';
+import './CardDesign.css';
+import { FaDownload } from "react-icons/fa";
+import QR from '../../assests/img/QR.png';
+import html2canvas from 'html2canvas';
+import { saveAs } from 'file-saver';
 
-export default CardDesign
+const CardDesign = () => {
+  const cardRef = useRef(null);
+
+  const handleDownload = () => {
+    html2canvas(cardRef.current).then(canvas => {
+      canvas.toBlob(blob => {
+        saveAs(blob, 'card_design.png');
+      });
+    });
+  };
+
+  return (
+    <div>
+      <div className="card" ref={cardRef}>
+        <div className="front-side">
+          <div className="color-grid">
+            <div className="color1"></div>
+            <div className="color2"></div>
+            <div className="color3"></div>
+            <div className="color4"></div>
+          </div>
+          <div className="info-grid">
+            <div className="name">
+              <h2>Abhishek Dangi</h2>
+              <h5>Full Stack Developer</h5>
+            </div>
+            <div className="addr">
+              <p>1/2 Street, <strong>City</strong>, State, <strong>Country</strong></p>
+            </div>
+            <div className="phoneNo">
+              <p>+000 <strong>1234</strong> 4567 7896</p>
+            </div>
+            <div className="emailId">
+              <p className="web"><strong>www</strong>.yourwebsite.<strong>com</strong></p>
+              <p className="email">dangiabhi332@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="back-side">
+          <div className="color-grid">
+            <div className="color1"></div>
+            <div className="color2"></div>
+            <div className="color3"></div>
+            <div className="color4"></div>
+          </div>
+          <div className="name-tag">
+            <img src={QR} alt="QR Code" className="qrcode" />
+          </div>
+        </div>
+      </div>
+      <button className="download" onClick={handleDownload}>
+        <FaDownload />
+        Download
+      </button>
+    </div>
+  );
+};
+
+export default CardDesign;
